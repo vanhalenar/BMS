ad2d999c13e60acde7c8254435b1a0ac2e33c791
vanhalenar <thalenar@gmail.com>
2025-11-03 21:52:00 +0100

    demodulation

--- main.cpp
+++ main.cpp
@@ -2,12 +2,33 @@
 #include <cmath>
 #include <cstring>
 #include <map>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 const double factor = 1/sqrt(10);
 
 map<string, int> grey = { {"00", -3}, {"01", -1}, {"10", 3}, {"11", 1} };
+map<int, string> grey_r = { {-3, "00"}, {-1, "01"}, {3, "10"}, {1, "11"} };
+
+
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        if (!empty(item))
+            *result++ = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
 
 void modulate(string num) {
     string s;
@@ -31,6 +52,19 @@ void modulate(string num) {
    } 
 }
 
+void demodulate(string num) {
+    double r = stod(num)/factor;
+    if (r < -2) {
+        cout<<"00";
+    } else if (r < 0) {
+        cout<<"01";
+    } else if (r < 2) {
+        cout<<"11";
+    } else {
+        cout<<"10";
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (strcmp(argv[1], "-m") == 0) {
         if (strcmp(argv[2], "-b") == 0) {
@@ -38,9 +72,28 @@ int main(int argc, char* argv[]) {
             modulate(number);
         }
         else if (strcmp(argv[2], "-f") == 0) {
-            cout<<"Not implemented, TODO"<<endl;
-            return 1;
+            ifstream f(argv[3]);
+            string number;
+            getline(f, number); //TODO multiline support?
+            modulate(number);
         } 
+    } else if (strcmp(argv[1], "-d") == 0) {
+        if (strcmp(argv[2], "-f") == 0) {
+            ifstream f(argv[3]);
+            string numbers[2];
+            string line;
+            while (getline(f, line)){
+                vector<string> numbs = split(line, ' ');
+                if (numbs.size()!=2) {
+                    continue;
+                }
+                string real = numbs[0];
+                string imag = numbs[1];
+                demodulate(real);
+                demodulate(imag);
+            }
+            cout<<endl;
+        }
     }
 
     return 0;

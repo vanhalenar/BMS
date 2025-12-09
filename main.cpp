1943b61c45621b0617da44eaae35d91264eadd79
vanhalenar <thalenar@gmail.com>
2025-12-09 22:33:33 +0100

    simple error handling

--- main.cpp
+++ main.cpp
@@ -8,42 +8,54 @@
 
 using namespace std;
 
+// Factor used for normalisation
 const double factor = 1/sqrt(10);
 
+// Grey code mappings
 map<string, int> grey = { {"00", -3}, {"01", -1}, {"10", 3}, {"11", 1} };
 map<int, string> grey_r = { {-3, "00"}, {-1, "01"}, {3, "10"}, {1, "11"} };
 
-template <typename Out>
-void split(const std::string &s, char delim, Out result) {
-    std::istringstream iss(s);
-    std::string item;
-    while (std::getline(iss, item, delim)) {
-        if (!empty(item))
-            *result++ = item;
+// https://cppscripts.com/cpp-split-string-by-delimiter
+vector<string> splitString(const string& str, char delimiter) {
+    vector<string> tokens;
+    stringstream ss(str);
+    string token;
+    while (getline(ss, token, delimiter)) {
+        if (!empty(token)) {
+            tokens.push_back(token);
+        }
     }
+    return tokens;
 }
 
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, std::back_inserter(elems));
-    return elems;
-}
-
+/**
+ * Modulate binary number into complex number.
+ *
+ * @param num Binary string, e.g. "1100", "10100011"
+ */
 void modulate(string num) {
     string s;
+
+    // Used to correct length of binary strings, so they're divisible by 4
     string zeros = "0000";
+    // Go through number in lengths of 4
     for (int i=0; i<num.size(); i+=4) {
         s = num.substr(i, 4);
         int size = s.size();
+        // Correct length of string
         if (size < 4) {
             s.append(zeros.substr(0, 4-size));
         }
 
+        // Find value in Grey mapping
         int number1 = grey[s.substr(0,2)];
         int number2 = grey[s.substr(2,2)];
+
+        // Multiply by factor
         double final1 = number1 * factor;
         double final2 = number2 * factor;
 
+        // Make output nicer
         char spacer1 = final1 > 0 ? ' ' : '\0';
         char spacer2 = final2 > 0 ? ' ' : '\0';
 
@@ -51,8 +63,16 @@ void modulate(string num) {
    } 
 }
 
+/**
+ * Demodulate decimal number into binary.
+ *
+ * Prints a 2-digit binary string to output.
+ * @param num Decimal number, e.g. "-0.948683, 0.316228"
+ */
 void demodulate(string num) {
+    // Divide by factor
     double r = stod(num)/factor;
+    // Print according to thresholds
     if (r < -2) {
         cout<<"00";
     } else if (r < 0) {
@@ -65,35 +85,53 @@ void demodulate(string num) {
 }
 
 int main(int argc, char* argv[]) {
+    // Modulating
     if (strcmp(argv[1], "-m") == 0) {
+        // From STDIN
         if (strcmp(argv[2], "-b") == 0) {
             string number(argv[3]);
             modulate(number);
         }
+        // From file, process all lines
         else if (strcmp(argv[2], "-f") == 0) {
             ifstream f(argv[3]);
             string number;
             while(getline(f, number)) {
                 modulate(number);
             }
-        } 
+        }
+        else {
+            fprintf(stderr, "Invalid modulation input mode");
+            return 1;
+        }
+    // Demodulating
     } else if (strcmp(argv[1], "-d") == 0) {
         if (strcmp(argv[2], "-f") == 0) {
             ifstream f(argv[3]);
-            string numbers[2];
             string line;
+            // Process each line
             while (getline(f, line)){
-                vector<string> numbs = split(line, ' ');
-                if (numbs.size()!=2) {
+                // Get real and imaginary parts
+                vector<string> numbers = splitString(line, ' ');
+                if (numbers.size()!=2) {
                     continue;
                 }
-                string real = numbs[0];
-                string imag = numbs[1];
+                // Process real and imaginary parts individually because it's the same
+                string real = numbers[0];
+                string imag = numbers[1];
                 demodulate(real);
                 demodulate(imag);
             }
             cout<<endl;
         }
+        else {
+            fprintf(stderr, "Invalid demodulation input mode");
+            return 1;
+        }
+    }
+    else {
+        fprintf(stderr, "Invalid mode");
+        return 1;
     }
 
     return 0;
